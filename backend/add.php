32b1aae84c9eeceecabbd81e84669e4e24568d51
Evy Kassirer <evy.kassirer@gmail.com>
2014-08-02 16:09:38 -0700

    add to review database

--- backend/add.php
+++ backend/add.php
@@ -18,8 +18,10 @@
 	$usefulness = trim(stripslashes($_POST['usefulness']));
 	$easiness = trim(stripslashes($_POST['easiness']));
 	$overall = $usefulness + $easiness;
-	if (easiness > 5)
+	if ($easiness > 5)
 		$overall = $usefulness + 10 - $easiness; 
+
+	$comments = trim(stripslashes($_POST['comments']));
 
 	// CHECK FOR URL DUPLICATES LATER
 	/*
@@ -34,4 +36,17 @@
 	$STH=$DBH->prepare("INSERT INTO courses VALUES ('', :name, :prereqs, :subjects, :url, :usefulness, :easiness, :overall)");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
 	$STH->execute(array(":name" => $name, ":prereqs" => $prereqs, ":subjects" => $prereqs, ":subjects" => $subjects, ":url" => $url, ":usefulness" => $usefulness, ":easiness" => $easiness, ":overall" => $overall));
+
+	$STH=$DBH->prepare("SELECT id FROM courses WHERE url = :url");
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+	$STH->execute(array(":url" => $url));
+	$result = $STH->fetchAll();
+	$course_id = $result[0]["id"];
+
+	// user will be based on their IP address
+	$user = 0;
+
+	$STH=$DBH->prepare("INSERT INTO reviews VALUES ('', :user, :course, :usefulness, :easiness, :comments)");
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+	$STH->execute(array(":user" => $user, ":course" => $course_id, ":usefulness" => $usefulness, ":easiness" => $easiness, ":comments" => $comments));
 ?>
