c24ad1445538bad15aace684597363cbcd8a6755
Dave Pagurek <dave@davepagurek.com>
2014-08-03 07:33:22 -0700

    Show Me More button works

--- match.js
+++ match.js
@@ -1,3 +1,13 @@
+var current = 0;
+
 window.onload = function() {
-    
+    var nextButtons = document.getElementsByClassName("secondary");
+    var features = document.getElementsByClassName("feature");
+    for (var i=0; i<nextButtons.length; i++) {
+        nextButtons[i].onclick = function() {
+            features[current].className = "feature hidden";
+            features[current+1].className = "feature";
+            current++;
+        };
+    }
 };
