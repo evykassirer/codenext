867486d66e8ef34d601fef9d695e495288aa9f62
Evy Kassirer <evy.kassirer@gmail.com>
2014-08-02 18:07:42 -0700

    updates easiness and usefulness scores when someone adds an existing course

--- add.php
+++ add.php
@@ -1,6 +1,8 @@
 <?php
 	require "login.php";
 	
+
+	// Get data from form
 	$name = trim(stripslashes($_POST['name']));
 
 	function FormatTagString($string) {
@@ -10,10 +12,8 @@
 		}
 		return implode(",", $tags);
 	}
-
 	$prereqs = trim(stripslashes($_POST['prereqs']));
 	$prereqs = FormatTagString($prereqs);
-
 	$subjects = trim(stripslashes($_POST['subjects']));
 	$subjects = FormatTagString($subjects);
 
@@ -30,34 +30,59 @@
 
 	$comments = trim(stripslashes($_POST['comments']));
 
-	// CHECK FOR URL DUPLICATES LATER
-	/*
+	// TODO: user will be based on their IP address or an id to a database, we make it 0 for now.
+	$user = 0;
+
+	// Check if the course is already in the database before updating it.
 	$STH=$DBH->prepare("SELECT * FROM courses WHERE url = :url");
-					$STH->setFetchMode(PDO::FETCH_ASSOC);
-					$STH->execute(array(":address" => $url));
-					$user_result=$STH->fetchAll();
-
-					if (count($user_result) != 0) {
-	*/
-
-	$STH=$DBH->prepare("INSERT INTO courses VALUES ('', :name, :prereqs, :subjects, :url, :usefulness, :easiness, :overall)");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
-	$STH->execute(array(":name" => $name, ":prereqs" => $prereqs, ":subjects" => $prereqs, ":subjects" => $subjects, ":url" => $url, ":usefulness" => $usefulness, ":easiness" => $easiness, ":overall" => $overall));
-
-	$STH=$DBH->prepare("SELECT id FROM courses WHERE url = :url");
+	$STH->execute(array(":url" => $url));
+	$course=$STH->fetchAll();
+	if (count($course) == 0) {
+		$overall = $usefulness + $easiness;
+		if ($easiness > 5)
+			$overall = $usefulness + 10 - $easiness; 
+		$STH=$DBH->prepare("INSERT INTO courses VALUES ('', :name, :prereqs, :subjects, :url, :usefulness, :easiness, :overall)");
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$STH->execute(array(":name" => $name, ":prereqs" => $prereqs, ":subjects" => $prereqs, ":subjects" => $subjects, ":url" => $url, ":usefulness" => $usefulness, ":easiness" => $easiness, ":overall" => $overall));
+	}
+	
+	// We always add a new review. 
+	// TODO: one review per user.
+	$STH=$DBH->prepare("SELECT * FROM courses WHERE url = :url");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
 	$STH->execute(array(":url" => $url));
 	$result = $STH->fetchAll();
 	$course_id = $result[0]["id"];
-
-	// user will be based on their IP address
-	$user = 0;
-
 	$STH=$DBH->prepare("INSERT INTO reviews VALUES ('', :user, :course, :usefulness, :easiness, :comments)");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
 	$STH->execute(array(":user" => $user, ":course" => $course_id, ":usefulness" => $usefulness, ":easiness" => $easiness, ":comments" => $comments));
 
-	// Add subject tags.
+	// If we added a new review to an existing course, update the course data.
+	if (count($course) != 0) {
+		$STH=$DBH->prepare("SELECT * FROM reviews WHERE course = :id");
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$STH->execute(array(":id" => $course_id));
+		$reviews=$STH->fetchAll();
+
+		$new_usefulness = 0;
+		$new_easiness = 0;
+		foreach ($reviews as $review) {
+			$new_usefulness += $review["usefulness"];
+			$new_easiness += $review["easiness"];
+		}
+		$new_usefulness /= count($reviews);
+		$new_easiness /= count($reviews);
+		$new_overall = $new_usefulness + $new_easiness;
+		if ($new_easiness > 5)
+			$new_overall = $new_usefulness + 10 - $new_easiness;
+		$STH=$DBH->prepare("UPDATE courses SET usefulness=:usefulness, easiness=:easiness, overall=:overall WHERE id=:id");
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$STH->execute(array(":usefulness" => $new_usefulness, ":easiness" => $new_easiness, ":overall" => $new_overall, ":id" => $course_id));
+	}
+
+
+	// Update supported subject tags.
 	$tags = explode(",", $subjects);
 	foreach ($tags as $tag) {
     	$STH=$DBH->prepare("SELECT 1 FROM tags WHERE name = :name");
@@ -72,7 +97,7 @@
 		}
 	}
 
-	// Add prerequisite tags in the same way.
+	// Update prerequisite tags in the same way.
 	$tags = explode(",", $prereqs);
 	foreach ($tags as $tag) {
     	$STH=$DBH->prepare("SELECT 1 FROM prereqs WHERE name = :name");
