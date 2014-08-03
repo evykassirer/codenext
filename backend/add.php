86e198757f2375d6295ddf6326a44e02b24556e6
Evy Kassirer <evy.kassirer@gmail.com>
2014-08-02 17:02:29 -0700

    updates tags in the database

--- backend/add.php
+++ backend/add.php
@@ -49,4 +49,36 @@
 	$STH=$DBH->prepare("INSERT INTO reviews VALUES ('', :user, :course, :usefulness, :easiness, :comments)");
 	$STH->setFetchMode(PDO::FETCH_ASSOC);
 	$STH->execute(array(":user" => $user, ":course" => $course_id, ":usefulness" => $usefulness, ":easiness" => $easiness, ":comments" => $comments));
+
+	// Add subject tags.
+	$tags = explode(",", $subjects);
+	foreach ($tags as $tag) {
+    	$STH=$DBH->prepare("SELECT 1 FROM tags WHERE name = :name");
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$STH->execute(array(":name" => $tag));
+		$result = $STH->fetchAll();
+		// Add the tag if it isn't in the database yet.
+		if (count($result) == 0) {
+			$STH=$DBH->prepare("INSERT INTO tags VALUES ('', :name)");
+			$STH->setFetchMode(PDO::FETCH_ASSOC);
+			$STH->execute(array(":name" => $tag));			
+		}
+	}
+
+	// Add prerequisite tags in the same way.
+	$tags = explode(",", $prereqs);
+	foreach ($tags as $tag) {
+    	$STH=$DBH->prepare("SELECT 1 FROM prereqs WHERE name = :name");
+		$STH->setFetchMode(PDO::FETCH_ASSOC);
+		$STH->execute(array(":name" => $tag));
+		$result = $STH->fetchAll();
+		// Add the tag if it isn't in the database yet.
+		if (count($result) == 0) {
+			$STH=$DBH->prepare("INSERT INTO prereqs VALUES ('', :name)");
+			$STH->setFetchMode(PDO::FETCH_ASSOC);
+			$STH->execute(array(":name" => $tag));			
+		}
+	}
+
+
 ?>
